a323aa2a7428d2f32d4284a2671e96a76a07bb99
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-24 23:23:01 +0300

    Add story domain package

---
+++ packages/stories/__init__.py
@@ -0,0 +1,15 @@
+from packages.stories.models import (
+    EpisodeStory,
+    StoryGenerationJobRead,
+    StoryGenerationRequest,
+    StoryJobStatus,
+)
+from packages.stories.service import StoryJobService
+
+__all__ = [
+    "EpisodeStory",
+    "StoryGenerationJobRead",
+    "StoryGenerationRequest",
+    "StoryJobService",
+    "StoryJobStatus",
+]
