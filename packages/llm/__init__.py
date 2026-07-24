d111b6163117fa9a949ee4e6cb69b54931344a67
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-24 23:22:02 +0300

    Add self-hosted LLM package

---
+++ packages/llm/__init__.py
@@ -0,0 +1,5 @@
+from packages.llm.models import LLMHealth, LLMMessage
+from packages.llm.openai_compatible import OpenAICompatibleLLMProvider
+from packages.llm.provider import LLMProvider
+
+__all__ = ["LLMHealth", "LLMMessage", "LLMProvider", "OpenAICompatibleLLMProvider"]
