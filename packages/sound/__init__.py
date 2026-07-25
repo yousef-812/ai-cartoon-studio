b10cef665bc4337c5090af939c72ddaddb0f5042
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-25 15:45:15 +0300

    Start sound design and music foundation

---
+++ packages/sound/__init__.py
@@ -0,0 +1,23 @@
+from packages.sound.models import (
+    SoundAsset,
+    SoundAssetType,
+    SoundCue,
+    SoundMixJobRead,
+    SoundMixJobStatus,
+    SoundMixPlanRequest,
+    SoundMixReviewRequest,
+    SoundMixReviewStatus,
+    SoundMixSpec,
+)
+
+__all__ = [
+    "SoundAsset",
+    "SoundAssetType",
+    "SoundCue",
+    "SoundMixJobRead",
+    "SoundMixJobStatus",
+    "SoundMixPlanRequest",
+    "SoundMixReviewRequest",
+    "SoundMixReviewStatus",
+    "SoundMixSpec",
+]
