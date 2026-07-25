027a42bcac6da7980a3e5456cd86b372a7107340
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-25 16:18:20 +0300

    Export finalization domain

---
+++ packages/finalization/__init__.py
@@ -0,0 +1,29 @@
+from packages.finalization.models import (
+    FinalArtifact,
+    FinalizationJobRead,
+    FinalizationJobSpec,
+    FinalizationJobStatus,
+    FinalizationPlanRequest,
+    FinalizationReviewRequest,
+    FinalizationReviewStatus,
+    QCCheck,
+    QCReport,
+    QCSeverity,
+    ShortCandidateSpec,
+    SubtitleCue,
+)
+
+__all__ = [
+    "FinalArtifact",
+    "FinalizationJobRead",
+    "FinalizationJobSpec",
+    "FinalizationJobStatus",
+    "FinalizationPlanRequest",
+    "FinalizationReviewRequest",
+    "FinalizationReviewStatus",
+    "QCCheck",
+    "QCReport",
+    "QCSeverity",
+    "ShortCandidateSpec",
+    "SubtitleCue",
+]
