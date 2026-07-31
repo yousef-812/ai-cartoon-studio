5cece0424a85efad3c7954a2a311f362f2195616
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-31 11:34:32 +0300

    Add torus quality helper

--- workers/blender/quality_torus.py
+++ workers/blender/quality_torus.py
@@ -1,1 +1,24 @@
+import math
+
 import bpy
+
+from quality_common import apply_material, smooth
+
+
+def torus(name, location, major_radius, minor_radius, value):
+    obj = bpy.data.objects.get(name)
+    if obj is None:
+        bpy.ops.mesh.primitive_torus_add(
+            major_radius=major_radius,
+            minor_radius=minor_radius,
+            major_segments=36,
+            minor_segments=10,
+            location=location,
+            rotation=(math.radians(90), 0, 0),
+        )
+        obj = bpy.context.object
+        obj.name = name
+    obj.location = location
+    smooth(obj)
+    apply_material(obj, value)
+    return obj
