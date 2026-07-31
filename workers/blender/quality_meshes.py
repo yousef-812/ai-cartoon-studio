68abf4d6383896194004463c7d308a2a522ad0f5
Yousef Yousry <yousefftaalip@gmail.com>
2026-07-31 11:33:07 +0300

    Add smooth sphere helper

--- workers/blender/quality_meshes.py
+++ workers/blender/quality_meshes.py
@@ -1,1 +1,20 @@
 import bpy
+
+from quality_common import apply_material, smooth
+
+
+def sphere(name, location, scale, value):
+    obj = bpy.data.objects.get(name)
+    if obj is None:
+        bpy.ops.mesh.primitive_uv_sphere_add(segments=32, ring_count=20, location=location)
+        obj = bpy.context.object
+        obj.name = name
+    obj.location = location
+    obj.scale = scale
+    bpy.context.view_layer.objects.active = obj
+    obj.select_set(True)
+    bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
+    obj.select_set(False)
+    smooth(obj)
+    apply_material(obj, value)
+    return obj
